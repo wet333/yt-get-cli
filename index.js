959ed32b91348d33fdbafc8a4a7895898472fe72
Agustin Wet <wet.4gustin@gmail.com>
2023-05-28 22:52:10 -0300

    added shebang to index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
+#!/usr/bin/env node
+
 const ytGet = require("yt-get");
 
 function main() {
+	const option = process.argv[2].toString();
+	const URL = process.argv[3].toString();
 
-    const option = process.argv[2].toString();
-    const URL = process.argv[3].toString();
+	if (option === "playlist") {
+		ytGet.downloadPlaylist(URL);
+	}
 
-    if (option === "playlist") {
-        ytGet.downloadPlaylist(URL);
-    }
-    
-    if (option === "video") {
-        ytGet.downloadVideo(URL);
-    }
-    
+	if (option === "video") {
+		ytGet.downloadVideo(URL);
+	}
 }
 
 main();
